7cab6f84e1440673a07908de9ee718c1aab9f6bb
regfd <neubauersimon@hotmail.com>
2020-02-04 23:11:34 -0500

    Blogger Section Edit and Flash Done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,8 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         @article.save
     
+        flash.notice = "Article '#{@article.title}' is now live!  "
+
         redirect_to article_path(@article)             #links to the
 
     end
@@ -25,6 +27,23 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         @article.destroy                                #destroys the article
 
+        flash.notice = "Article '#{@article.title}' is Deleted!"
+
         redirect_to articles_path                   #Links to the articles overview
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id]) 
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
+    end
+
+
 end

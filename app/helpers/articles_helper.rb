e3179663e319fc127176698f318fa6af5f1fd78c
regfd <neubauersimon@hotmail.com>
2020-02-06 22:07:45 -0500

    : #AAA;
    body {
      background-color: ;
      font: {
        family: Verdana, Helvetica, Arial;
        size: 14px;
      }
    }
    
    a {
      color: #0000FF;
      img {
        border: none;
      }
    }
    
    .clear {
      clear: both;
      height: 0;
      overflow: hidden;
    }
    
    #container {
      width: 75%;
      margin: 0 auto;
      background: #fff;
      padding: 20px 40px;
      border: solid 1px black;
      margin-top: 20px;
    }
    
    #content {
      clear: both;
      padding-top: 20px;

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,6 @@
 module ArticlesHelper
 
     def article_params
-        params.require(:article).permit(:title, :body, :tag_list)
-      end
-
+      params.require(:article).permit(:title, :body, :tag_list, :image, :image_file_name)
+  	end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,10 @@ class Article < ApplicationRecord
     has_many :tags, through: :taggings
 
 
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+
     def tag_list
       self.tags.collect do |tag|
         tag.name
@@ -19,5 +23,6 @@ class Article < ApplicationRecord
 
     
 end
+
 
 
